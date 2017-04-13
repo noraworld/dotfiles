c201cbb6a63b9456d8bf70b08cc2c252a04b1cd3
noraworld <noraworld@mail.com>
2017-04-13 20:37:07 +0900

    :sparkles: Setup: check file or directory existence strictly

--- setup.sh
+++ setup.sh
@@ -10,36 +10,41 @@ if [ ${0} != "setup.sh" ]; then
   exit 1
 fi
 
-for path in $PWD/*
+function link_file() {
+  if [ -d ${2} ] || [ -f ${2} ] && [ ! -L ${2} ] || [ -L ${2} ] && [ "`readlink ${2}`" != "${1}" ]; then
+    echo "${2} already exists. You need to rename or move ${2} to another filename or directory.\n"
+    echo "Skipping."
+  elif [ ! -e ${2} ]; then
+    ln -s ${1} ${2}
+  fi
+}
+
+function link_directory() {
+  if [ -f ${2} ] || [ -d ${2} ] && [ ! -L ${2} ] || [ -L ${2} ] && [ "`readlink ${2}`" != "${1}" ]; then
+    echo "${2} already exists. You need to rename or move ${2} to another filename or directory.\n"
+    echo "Skipping."
+  elif [ ! -e ${2} ]; then
+    ln -s ${1} ${2}
+  fi
+}
+
+# link all files in current directory
+for src_path in $PWD/*
 do
-  if [ -f ${path} ]; then
-    file=`basename ${path}`
-
-    if [ -e ${HOME}/.${file} -a -e ${HOME}/.${file}.backup ]; then
-      echo "Error: this script tried to rename ${HOME}/.${file} to ${HOME}/.${file}.backup not to overwrite the original config file, but ${HOME}/.${file}.backup already exists.\n"
-      echo "This operation was aborted.\n"
-      continue
-    fi
-
-    if [ -e ${HOME}/.${file} ]; then
-      mv ${HOME}/.${file} ${HOME}/.${file}.backup
-    fi
-    ln -s ${path} ${HOME}/.${file}
+  if [ -f ${src_path} ]; then
+    dst_path=${HOME}/.`basename ${src_path}`
+    link_file ${src_path} ${dst_path}
   fi
 done
 
-if [ ! -e $HOME/.bash ]; then
-  ln -s ${PWD}/bash $HOME/.bash
-fi
-
+# create .vim/ directory if not exists
 if [ ! -e $HOME/.vim ]; then
   mkdir $HOME/.vim
 fi
 
-if [ ! -e $HOME/.vim/config ]; then
-  ln -s $PWD/vim $HOME/.vim/config
-fi
+# link all files in bash/ and vim/ directory
+link_directory ${PWD}/bash $HOME/.bash
+link_directory $PWD/vim $HOME/.vim/config
 
 # unlink this script and load config files
 unlink $HOME/.setup.sh
-source $HOME/.bash_profile
