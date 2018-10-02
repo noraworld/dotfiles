78918388293a864e1ef8677ab741acce0be19707
noraworld <mail@noraworld.com>
2018-10-02 15:11:55 +0900

    Add GNU sed PATH

--- bash/macos.bash
+++ bash/macos.bash
@@ -10,9 +10,16 @@ if [ -f `brew --prefix`/etc/bash_completion ]; then
 fi
 
 # Set the environment variables
-PATH=/usr/local/sbin:$PATH                                  # Homebrew
+PATH="/usr/local/sbin:$PATH"                                # Homebrew
+
 PATH="/usr/local/opt/coreutils/libexec/gnubin:$PATH"        # GNU coreutils
 MANPATH="/usr/local/opt/coreutils/libexec/gnuman:$MANPATH"  # GNU coreutils manual
+
 PATH="/usr/local/opt/findutils/libexec/gnubin:$PATH"        # GNU findutils
 MANPATH="/usr/local/opt/findutils/libexec/gnuman:$MANPATH"  # GNU findutils manual
+
 PATH="/usr/local/opt/gnu-getopt/bin:$PATH"                  # GNU getopt
+MANPATH="/usr/local/opt/gnu-getopt/share/man:$MANPATH"      # GNU getopt manual
+
+PATH="/usr/local/opt/gnu-sed/libexec/gnubin:$PATH"          # GNU sed
+MANPATH="/usr/local/opt/gnu-sed/libexec/gnuman:$MANPATH"    # GNU sed manual
