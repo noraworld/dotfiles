a31f164c81536325ed8a84471f404b542d8f403e
noraworld <mail@noraworld.com>
2021-10-14 11:49:25 +0900

    Turn ^F into smart forward
    If suggested words are showing up, then it behaves
    as "forward-word", which accept only one word
    
    Otherwise, in short, if suggested words are not
    showing up, then it behaves as "forward-char",
    which is the normal ^F behavior

--- zsh/bindkey.zsh
+++ zsh/bindkey.zsh
@@ -3,3 +3,4 @@
 bindkey -e # use emacs-like key bindings
 
 bindkey '^J' __fuzzy_push_directory
+bindkey '^F' __smart_forward
--- zsh/zle.zsh
+++ zsh/zle.zsh
@@ -11,3 +11,17 @@ function __fuzzy_push_directory() {
 }
 
 zle -N __fuzzy_push_directory
+
+function __smart_forward() {
+  if [ -n "$POSTDISPLAY" ]; then
+    zle forward-word
+
+    # "reset-prompt" is necessary because suggested words that have not been typed yet show up with a normal color
+    # as if they have already been accepted somehow...
+    zle reset-prompt
+  else
+    zle forward-char
+  fi
+}
+
+zle -N __smart_forward
