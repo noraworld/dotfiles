7e329a707e34bc84dcba3b85277675cfeb48b734
noraworld <mail@noraworld.com>
2021-10-18 14:12:04 +0900

    Speed up directory stack loading at startup
    When loading only path.zsh and dstack.zsh:
      before: 0.1206338406 sec
      after:  0.0266859531 sec
    
    When loading all Zsh configurations:
      before: 0.3329880237 sec
      after:  0.2396841049 sec

--- zsh/dstack.zsh
+++ zsh/dstack.zsh
@@ -3,25 +3,10 @@
 # Restore directory stack when shell session starts or restarts
 # Also see store_directory_stack
 #
-# CAUTION: This should be loaded after ./zsh/bin.zsh is loaded
-if [[ -e $HOME/.directory_stack_store ]]; then
-  if [[ $(dirs -v | wc -l) -eq 1 ]]; then
-    while read line
-    do
-      # expand tilde into home directory’s path
-      # somehow tilde is not expanded
-      #
-      # NOTE:
-      #   use `|` instead of `/` as sed separator
-      #   because `/` overlaps with path expression
-      file=$(echo $line | sed -e "s|^~|$HOME|g")
-
-      pushd $file 1>/dev/null
-    done << FILE
-$(tac $HOME/.directory_stack_store || tail -r $HOME/.directory_stack_store)
-FILE
-
-    # delete directory of when shell session starts from stack
-    popd -0 1>/dev/null
-  fi
+# NOTE: This should be loaded after zsh/path.zsh is loaded
+#       because it uses GNU sed and GNU tac
+#
+if [ -e $HOME/.directory_stack_store ] && [[ "$(dirs -v | wc -l)" -eq 1 ]]; then
+  eval $(tac $HOME/.directory_stack_store | sed -r "s|^[0-9]+\t(.*?)|pushd \"\1\" 1>/dev/null;|g")
+  popd -0 1>/dev/null # delete directory of when shell session starts from stack
 fi
