255d7a97772afefcd1fce2e503e4b05975c18e6c
noraworld <mail@noraworld.com>
2022-03-25 17:04:40 +0900

    Turn aliases into zabrze

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -2,27 +2,6 @@
 
 # General
 alias sudo='sudo ' # enable all aliases also at sudo mode
-alias reload='source ~/.zshrc'
-alias restart='exec -l $SHELL'
-alias del='trash'
-
-# Overridden
-alias ls='ls --color=auto -N'
-alias grep='grep --color=auto'
-alias script='script -a'
-# WARNING:
-#   Do not override commands unduly
-#   because some commands may not work properly
-#   by overriding the default behaviors
-
-# Abbreviation
-alias d='show_directory_stack'
-alias p='push_directory'
-alias pop='pop_directory'
-alias be='bundle exec'
-alias x='xargs -n 1 -o' # usage: which cat | x file
-alias ca='cron-apply'
-alias ifp='internet-fasting-period'
 
 if [[ "${OSTYPE}" =~ "linux*" ]]; then
   # https://github.com/sharkdp/bat/blob/994c21a5e14588dd252f1503ae06683baf8f3ffa/README.md#:~:text=Important%3A%20If%20you,with%20other%20distributions
